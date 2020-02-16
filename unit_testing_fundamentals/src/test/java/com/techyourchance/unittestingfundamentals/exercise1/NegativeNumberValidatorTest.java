19ea62ffcdd7dfe500b17e070f9c6d28dc2035c4
Wafaa-AbdElHafez-Ahmed <wafaa.abdelhafez-ahmed@vodafone.com>
2020-02-16 16:57:59 +0200

    Solved the first Exercise

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,37 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup(){
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1(){
+        boolean res = SUT.isNegative(-1);
+        Assert.assertThat(res, is(true));
+    }
+
+    @Test
+    public void test2(){
+        boolean res = SUT.isNegative(0);
+        Assert.assertThat(res, is(false));
+    }
+
+    @Test
+    public void test3(){
+        boolean res = SUT.isNegative(2);
+        Assert.assertThat(res, is(false));
+    }
+
 
 }
